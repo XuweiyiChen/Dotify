e7a9338514b7ab11c3f05f62580f6e4b402e61c5
“xuweic” <xuweic@uw.edu>
2020-04-13 00:09:37 -0700

    "update for hw1"

--- app/src/main/java/com/xuweic/dotify/MainActivity.kt
+++ app/src/main/java/com/xuweic/dotify/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.xuweic.dotify
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Color.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
-
 class MainActivity : AppCompatActivity() {
 
     private val randomNumber = Random.nextInt(0, 999999999)
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,8 +22,29 @@ class MainActivity : AppCompatActivity() {
         tvPlayTimes.text = "$randomNumber plays"
         val tvName = findViewById<EditText>(R.id.tvName)
         tvName.isEnabled = false;
+        val ivAlbum = findViewById<ImageView>(R.id.ivAlbum)
+        val tvSong = findViewById<TextView>(R.id.tvSong)
+        val singer = findViewById<TextView>(R.id.singer)
+        var toggleColor = true;
+        ivAlbum.setOnLongClickListener{
+            toggleColor = if (toggleColor) {
+                tvName.setTextColor(RED)
+                tvSong.setTextColor(RED)
+                singer.setTextColor(RED)
+                tvPlayTimes.setTextColor(RED)
+                false
+            } else {
+                tvName.setTextColor(BLACK)
+                tvSong.setTextColor(BLACK)
+                singer.setTextColor(BLACK)
+                tvPlayTimes.setTextColor(BLACK)
+                true
+            }
+            true
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     fun addPlayTimes(view: View) {
         val tvPlayTimes = findViewById<TextView>(R.id.tvPlayTimes)
         val input = tvPlayTimes.text.split(" ")
@@ -59,4 +79,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
 
